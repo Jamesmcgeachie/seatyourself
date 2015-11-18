3bc904785e05ebf56d50732015a1bf65e097a1cd
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-18 16:14:18 -0500

    Renamed users and sessions controllers to be plural

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+  before_action :get_user, only: [:show, :edit, :update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to new_restaurant_path, notice: "Successfully signed up for a Seat Yourself account."
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to root_path, notice: "Successfully updated your user account"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+end
