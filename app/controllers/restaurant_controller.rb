2bd32d8f25a4bfb80adad4664b68e48a90410d0a
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-18 10:31:37 -0500

    Created Restaurant Controller

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,43 @@
+class RestaurantController < ApplicationController
+
+
+before_action :get_restaurant, only: [:show, :edit, :update]
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path, notice: "Successfully signed up for a Seat Yourself account."
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to root_path, notice: "Successfully updated your restaurant account"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :summary, :street_number, :street_name, :city, :province, :postal_code, :country, :phone_number, :website_url)
+  end
+
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
+
+end
