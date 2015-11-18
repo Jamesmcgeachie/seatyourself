a5a1c18aec2b1b5500afa397c4ec0fdb0aed0845
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-17 19:03:00 -0500

    Created migration to change password column to password_digest column for has_secure_password method to use

---
+++ db/migrate/20151118000014_change_user_password_columnto_password_digest.rb
@@ -0,0 +1,5 @@
+class ChangeUserPasswordColumntoPasswordDigest < ActiveRecord::Migration
+  def change
+  	rename_column :users, :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117212648) do
+ActiveRecord::Schema.define(version: 20151118000014) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 20151117212648) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
