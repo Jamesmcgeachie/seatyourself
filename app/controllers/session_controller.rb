d61964504affaabe8fc4bc407eb3b5d89be75460
James McGeachie <jamesmcgeachie10@gmail.com>
2015-11-18 15:36:27 -0500

    Wrote session controller logic to handle user login (new and create) and user logout (destroy)

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+  		user = User.find_by(email: params[:email])
+  		if user && user.authenticate(params[:password])
+  			session[:user_id] = user.id
+  			redirect_to restaurants_url, notice: "Welcome #{user.first_name}, you logged in successfully."
+  		else
+  			flash[:alert] = "Invalid email or password!"
+  			render :new
+  		end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to restaurants_url, notice: "You have logged out of Seat Yourself."
+  end
+end
