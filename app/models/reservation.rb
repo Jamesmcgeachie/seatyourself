ebcec2709ec5f21c75875defaf2fa1d883141f85
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-20 14:06:12 -0500

    Changed Model Reservation for validation seating_time_cannot_be_in_the_past

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   validates :party_size, :seating_time, presence: true 
   validates :party_size, numericality: {only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 20}
+  validate :seating_time_cannot_be_in_the_past
+
+  def seating_time_cannot_be_in_the_past
+  	if  seating_time.present?  
+  		errors.add(:seating_time, "reservation can't be in the past") if seating_time < Date.today
+      	errors.add(:seating_time, "reservation should be on the hour") if seating_time.min != 0
+      	errors.add(:seating_time, "reservation should be between 11am and 8pm") if !(seating_time.hour).between?(11,20) 
+  	end
+  end 
+
 end
