d75e3a80ade2bf3624f44d3b537872fa2714c2a6
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-20 15:56:04 -0500

    Added Capacity Validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,8 @@ class Reservation < ActiveRecord::Base
   validates :party_size, :seating_time, presence: true 
   validates :party_size, numericality: {only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 20}
   validate :seating_time_cannot_be_in_the_past
+  validate :capacity_exists
+
 
   def seating_time_cannot_be_in_the_past
   	if  seating_time.present?  
@@ -12,5 +14,17 @@ class Reservation < ActiveRecord::Base
       	errors.add(:seating_time, "reservation should be between 11am and 8pm") if !(seating_time.hour).between?(11,20) 
   	end
   end 
+
+  def available?
+	Reservation.where(seating_time: self.seating_time, restaurant_id: self.restaurant_id ).sum(:party_size) + party_size <= self.restaurant.capacity
+  end
+
+  def capacity_exists
+  	unless available?
+  		errors.add(:party_size, "Party size would exceed capacity for this time slot")
+  	end
+
+  end
+
 
 end
